8a7e11271feb4cd4b701712069d1aea43f9bfe3d
Lithium@87 <cibs@abv.bg>
2024-01-04 19:07:06 +0200

    Change the label type and line thickness, correct the plot, use built-in styles, plot and style series of points with scatter()

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,15 +1,15 @@
 import matplotlib.pyplot as plt
 
-input_values = [1, 2, 3, 4, 5]
-squares = [1, 4, 9, 16, 25]
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
 
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.plot(input_values, squares, linewidth=3)
+ax.scatter(x_values, y_values, s=100)
 
 # Set chart title and label axes
-ax.set_title("Square Numbers", fontsize=24)
-ax.set_xlabel("Value", fontsize=14)
+ax.set_title("Square Number", fontsize=24)
+ax.set_xlabel("Values", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 
 # Set size of tick labels
@@ -17,3 +17,4 @@ ax.tick_params(labelsize=14)
 
 
 plt.show()
+# p.308
